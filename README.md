980d88d3d0fe0522e5c81155a6e6297da59d748b
Popkov Ilya <krik3ukr@gmail.com>
2021-05-05 05:42:55 +0300

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+# Courses Catalogue (CCatalogue)
+
+This is a simple web application which supports CRUD operations with the Catalogue of Courses.
+It is based on Flask framework.
+
+## Dependencies
+
+List of dependencies and required modules
+
+## Preset
+
+Set up Flask-related environment variables:
+
+```bash
+export FLASK_APP=ccatalogue
+export FLASK_ENV=development
+```
+
+Initialize the database
+
+```bash
+flask init-db
+```
+
+## Run
+
+Run the application:
+
+```bash
+flask run
+```
+
+It will start the web-applicaiton server on port 5000:
+
+http://127.0.0.1:5000
+
+## Test
+
+Steps to run unit tests
+
---
+++ ccatalogue/courses_controller.py
@@ -0,0 +1,79 @@
+from flask import Blueprint, request, jsonify, make_response
+from ccatalogue.db import get_db
+
+bp = Blueprint('courses', __name__, url_prefix='/courses')
+
+
+@bp.route('/', methods=['GET'])
+def get_courses():
+    db = get_db()
+    name = request.args.get('name')
+    if name:
+        courses = db.execute('SELECT * FROM catalogue WHERE course_name = ?', (name,)).fetchone()
+        return make_response(jsonify([dict(courses)]), 200)
+    date = request.args.get('date')
+    if date:
+        courses = db.execute('SELECT * FROM catalogue WHERE start_date = ?', (date,)).fetchall()
+        return make_response(jsonify([dict(ix) for ix in courses]), 200)
+    courses = db.execute('SELECT * FROM catalogue').fetchall()
+    return make_response(jsonify([dict(ix) for ix in courses]), 200)
+
+
+@bp.route('/', methods=['POST'])
+def create_course():
+    data = request.get_json()
+    course_name = data['course_name']
+    start_date = data['start_date']
+    end_date = data['end_date']
+    lectures_number = data['lectures_number']
+    db = get_db()
+    cur = db.cursor()
+    try:
+        cur.execute('INSERT INTO catalogue (course_name, start_date, end_date, lectures_number)'
+                    ' VALUES (?, ?, ?, ?)',
+                    (course_name, start_date, end_date, lectures_number)
+                    )
+        db.commit()
+    except Exception as e:
+        print(e)
+    return make_response(jsonify({"id": cur.lastrowid}), 200)
+
+
+@bp.route('/<id>', methods=['PUT'])
+def update_course(id):
+    data = request.get_json()
+    course_name = data['course_name']
+    start_date = data['start_date']
+    end_date = data['end_date']
+    lectures_number = data['lectures_number']
+    db = get_db()
+    cur = db.cursor()
+    try:
+        cur.execute('UPDATE catalogue SET course_name = ?, start_date = ?, end_date = ?, lectures_number = ?'
+                    ' WHERE id = ?',
+                    (course_name, start_date, end_date, lectures_number, id)
+                    )
+        db.commit()
+    except Exception as e:
+        print(e)
+    return make_response(jsonify({"id": id}), 200)
+
+
+@bp.route('/<id>', methods=['GET'])
+def get_course_by_id(id):
+    db = get_db()
+    course = db.execute('SELECT * FROM catalogue WHERE id = ?', (id,)).fetchone()
+    resp = dict(course) if course else {}
+    return make_response(jsonify(resp), 200)
+
+
+@bp.route('/<id>', methods=['DELETE'])
+def delete_course_by_id(id):
+    db = get_db()
+    cur = db.cursor()
+    try:
+        cur.execute('DELETE FROM catalogue WHERE id = ?', (id,)).fetchone()
+        db.commit()
+    except Exception as e:
+        print(e)
+    return make_response(jsonify({"id": id}), 200)
